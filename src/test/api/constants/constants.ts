acd15b71293b15022185ed6c90f61734776fd32b
Satz-TestingQA <satheeshkumarnedumaran@gmail.com>
2025-09-14 00:17:00 +0530

    test runs

--- src/test/api/constants/constants.ts
+++ src/test/api/constants/constants.ts
@@ -1,6 +1,6 @@
 export enum testConstants{
 QaConfigFilePath = 'src/test/api/config/qaConfig.json', 
-ProdConfigFilePath = 'src/test/api/config/ProdConfig.json',
+ProdConfigFilePath = 'src/test/api/config/prodConfig.json',
 Encoding = 'utf8',
 QaEnvironment = 'QA',
 ProdEnvironment = 'PROD',
